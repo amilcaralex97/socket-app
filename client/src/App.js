b606ca1ca332f6010db8cf454250af9fd04bd7ae
Amilcar Sanchez <amillcar.sanchez@wizeline.com>
2021-11-10 23:38:57 -0600

    chat preview and client connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
-import styled from "styled-components";
-import { ChatContainer } from "./container/ChatContainer/ChatContainer";
+import io from "socket.io-client";
 
-const AppContainer = styled.div`
-  height: 100vh;
-`;
+const socket = io.connect("http://localhost:5001");
 
 const App = () => {
+  const [username, setusername] = useState("");
+  const [room, setroom] = useState("");
+
+  const joinRoom = () => {};
   return (
-    <AppContainer>
-      <ChatContainer />
-    </AppContainer>
+    <div>
+      <h3>Join A chat</h3>
+      <input type="text" placeholder="John..." />
+      <input type="text" placeholder="Room ID..." />
+      <button>Join A Room</button>
+    </div>
   );
 };
 
--- server/index.js
+++ server/index.js
@@ -17,7 +17,11 @@ const io = new Server(server, {
 });
 
 io.on('connection', (socket) => {
-    console.log(socket.id);
+    console.log(`User connected ${socket.id}`);
+
+    io.on('disconnect', () => {
+        console.log('User disconnected');
+    });
 });
 
 const PORT = process.env.PORT || 5001;
