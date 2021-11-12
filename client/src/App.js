c0e4d031c8a66670e2c2b6257a1450a1fb55d522
Amílcar Sánchez <amilcaralex97@gmail.com>
2021-11-12 00:09:40 -0600

    socket error handling

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,66 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Chat from "./Chat";
 import io from "socket.io-client";
 
-const socket = io.connect("http://localhost:5001");
+const socket = io.connect("http://192.168.100.9:5001/");
 
 const App = () => {
   const [username, setUsername] = useState("");
   const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
+  const [socketError, setSocketError] = useState("");
+  const [welcome, setWelcome] = useState("");
+
+  useEffect(() => {
+    socket.on("connect_error", (data) => {
+      console.log(data.message);
+      setSocketError(data.message);
+    });
+
+    socket.on("welcome", (data) => {
+      setWelcome(data);
+    });
+  }, [socket]);
 
   const joinRoom = () => {
     if (username !== "" && room !== "") {
       socket.emit("join_room", room);
+      setShowChat(true);
     }
   };
-  return (
-    <div className="App">
-      <div className="joinChatContainer">
-        <h3>Join A chat</h3>
-        <input
-          type="text"
-          placeholder="John..."
-          onChange={(event) => {
-            setUsername(event.target.value);
-          }}
-        />
-        <input
-          type="text"
-          placeholder="Room ID..."
-          onChange={(event) => {
-            setRoom(event.target.value);
-          }}
-        />
-        <button onClick={joinRoom}>Join A Room</button>
-      </div>
 
-      <Chat socket={socket} username={username} room={room} />
-    </div>
-  );
+  if (!socketError) {
+    return (
+      <div className="App">
+        <h2>{welcome}</h2>
+        {!showChat ? (
+          <div className="joinChatContainer">
+            <h3>Join A chat</h3>
+            <input
+              type="text"
+              placeholder="John..."
+              onChange={(event) => {
+                setUsername(event.target.value);
+              }}
+            />
+            <input
+              type="text"
+              placeholder="Room ID..."
+              onChange={(event) => {
+                setRoom(event.target.value);
+              }}
+            />
+            <button onClick={joinRoom}>Join A Room</button>
+          </div>
+        ) : (
+          <Chat socket={socket} username={username} room={room} />
+        )}
+      </div>
+    );
+  }
+
+  return <h1>Error while trying to connect</h1>;
 };
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -11,14 +11,14 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:3000',
+        origin: '*',
         methods: ['GET', 'POST'],
     },
 });
 
 io.on('connection', (socket) => {
     console.log(`User connected ${socket.id}`);
-
+    socket.emit('welcome', `Welcome to the socket ${socket.id}`);
     socket.on('join_room', (data) => {
         socket.join(data);
         console.log(`user with ID ${socket.id} joined room: ${data}`);
